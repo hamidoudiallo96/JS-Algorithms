7f16040806b68a8d721d02c39030bc2d3d63c7ca
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-03-06 21:44:09 -0500

    solved

---
+++ find_second_neg.js
@@ -0,0 +1,29 @@
+// In a given array find the second to last negative number in an array
+// For example in [1,4,-5,9,-9] => -5
+
+// inputs => array
+// outputs => negative number, second to last
+
+const findSecondLastNeg = arr => {
+	// create a set to store negative numbers
+	const negSet = new Set();
+	let arrNum = [];
+	// loop over array
+	for (let num of arr) {
+		let isNum = Math.sign(num);
+		// if num is negative add it to the set
+		if (isNum === -1) {
+			negSet.add(num);
+		}
+	}
+
+	for (let num of negSet.values()) {
+		arrNum.push(num);
+	}
+	// return second to last negative number
+	let secondToLastNum = arrNum[arrNum.length - 2];
+	return secondToLastNum;
+};
+
+// test cases
+console.log(findSecondLastNeg([4, -4, 2, 10, -16]));
